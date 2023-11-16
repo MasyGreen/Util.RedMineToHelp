8895481887566acf01436178881505dd7360dddb
masygreen <masygreen@gmail.com>
2023-11-16 12:03:22 +0300

    to json config

--- RedMineDownload.py
+++ RedMineDownload.py
@@ -1,4 +1,4 @@
-import configparser
+import json
 import os
 import re
 from lxml import html
@@ -13,56 +13,53 @@ from redminelib import Redmine
 # Read config
 def ReadConfig(filepath):
     print(f'{Fore.YELLOW}Start ReadConfig')
+    try:
+        if os.path.exists(filepath):
+            json_from_file: str = ""
+            with open(filepath, 'r', encoding='utf-8-sig') as file:
+                json_from_file = file.read()
 
-    if os.path.exists(filepath):
-        config = configparser.ConfigParser()
-        config.read(filepath, "utf8")
-        config.sections()
+            settings_json = json.loads(json_from_file)
 
-        global glhost
-        glhost = config.has_option("Settings", "host") and config.get("Settings", "host") or None
+            global glhost
+            glhost = settings_json.get('host', None)
 
-        global glapikey
-        glapikey = config.has_option("Settings", "apikey") and config.get("Settings", "apikey") or None
+            global glapikey
+            glapikey = settings_json.get('apikey', None)
 
-        global glid
-        glid = config.has_option("Settings", "id") and config.get("Settings", "id") or None
+            global glid
+            glid = settings_json.get('list', [])
 
-        global glclear
-        str = config.has_option("Settings", "clear") and config.get("Settings", "clear") or None
-        glclear = False
+            global glclear
+            glclear = settings_json.get('clear', False)
 
-        if str is not None:
-            if str == '1' or str.lower() == 'true':
-                glclear = True
+            global gllogon
+            gllogon = settings_json.get('logon', 0)
 
-        global gllogon
-        str = config.has_option("Settings", "logon") and config.get("Settings", "logon") or None
-        gllogon = 0
-
-        if str is None:
-            gllogon = 0
-        if not str.isdigit():
-            gllogon = 0
+            return True
         else:
-            gllogon = int(str)
+            print(f'{Fore.YELLOW}Start create config')
+            content_json = '''
+            {
+                "host": "http://192.168.1.1",
+                "apikey": "dq3inqgnqe8igqngninkkvekmviewrgir9384",
+                "clear": false,
+                "logon": 0,
+                "list": [
+                    "1677",
+                    "318",
+                    "id103/wiki/Help2"
+                ]
+            }
+            '''
 
-        return True
-    else:
-        print(f'{Fore.YELLOW}Start create config')
-        config = configparser.ConfigParser()
-        config.add_section("Settings")
+            with open(filepath, 'w', encoding='utf-8-sig') as file:
+                file.write(content_json)
 
-        config.set("Settings", "host", 'http://192.168.1.1')
-        config.set("Settings", "apikey", 'dq3inqgnqe8igqngninkkvekmviewrgir9384')
-        config.set("Settings", "id", '1677;318;id103/wiki/Help2')
-        config.set("Settings", "clear", 'False')
-        config.set("Settings", "logon", '0')
-
-        with open(filepath, "w") as config_file:
-            config.write(config_file)
-
-        print(f'{Fore.GREEN}Create config: {Fore.BLUE}{filepath},{config_file}')
+            print(f'{Fore.GREEN}Create config: {Fore.BLUE}{filepath}\n{content_json}')
+            return False
+    except Exception as err:
+        print(f'{Fore.RED}Delete config file, struct error...\n{err}')
         return False
 
 
@@ -105,42 +102,42 @@ def ClearRedmineLink(description):
 
 
 #  Get table and create file
-def ProcessDescription(downloadDirectory, description):
+def ProcessDescription(download_directory, description):
     description = ClearDescription(description)
     description = f'<html><body>{description}</body></html>'
 
     # 1 Получаем таблицы
-    htmlContent = html.fromstring(description)
-    tables = htmlContent.xpath(".//table")  # таблицы документа
+    html_content = html.fromstring(description)
+    tables = html_content.xpath(".//table")  # таблицы документа
     for table in tables:
         if gllogon >= 1:
-            htmlTablest = html.tostring(table, encoding='unicode')
+            html_tablest = html.tostring(table, encoding='unicode')
             print(f'{Fore.BLUE}------------------TABLE------------------------')
             if gllogon >= 2:
-                print(f'{Fore.WHITE}{htmlTablest}')
+                print(f'{Fore.WHITE}{html_tablest}')
 
-        rowList = table.xpath(".//tr")  # строки таблицы
+        row_list = table.xpath(".//tr")  # строки таблицы
         rInd = 0
         isSkip: bool = True
-        headRow = ""  # Заголовок таблицы
-        for row in rowList:
+        head_row = ""  # Заголовок таблицы
+        for row in row_list:
             rInd = rInd + 1
 
-            htmlRow = html.tostring(row, encoding='unicode')
-            htmlRow = ClearRedmineLink(htmlRow)
+            html_row = html.tostring(row, encoding='unicode')
+            html_row = ClearRedmineLink(html_row)
 
             if gllogon >= 1:
                 print(f'{Fore.BLUE}------------------ROW {rInd}------------------------')
                 if gllogon >= 2:
-                    print(f'{Fore.WHITE}{htmlRow}')
+                    print(f'{Fore.WHITE}{html_row}')
 
-            colList = row.xpath(".//th")  # все столбцы строки
-            if len(colList) == 0:
-                colList = row.xpath(".//td")  # все столбцы строки
+            col_list = row.xpath(".//th")  # все столбцы строки
+            if len(col_list) == 0:
+                col_list = row.xpath(".//td")  # все столбцы строки
 
             Article = ""
-            if len(colList) > 0:
-                Article = html.tostring(colList[0], encoding='unicode')
+            if len(col_list) > 0:
+                Article = html.tostring(col_list[0], encoding='unicode')
                 Article = Article.strip('\n')
                 Article = Article.strip('\r')
                 Article = Article.strip('\t')
@@ -153,49 +150,49 @@ def ProcessDescription(downloadDirectory, description):
                     print(f'{Fore.WHITE}Первая строка, первый столбец: {Article=}')
                 if Article.find("HelpID") > 0:
                     isSkip = False
-                    headRow = htmlRow
+                    head_row = html_row
             if isSkip:
                 print(f'{Fore.RED}Первый столбец первой строки не HelpID')
                 break
 
             # 3 Разбиваем таблицу построчно по наличию HelpID и записываем в файл
             if rInd > 1:
-                FindIDArticle = ClearDescription(Article)
-                ttd = re.findall('<.+?>', FindIDArticle)  # убираем <td>
+                find_id_article = ClearDescription(Article)
+                ttd = re.findall('<.+?>', find_id_article)  # убираем <td>
                 if len(ttd) < 2:
-                    print(f'{Fore.RED}{FindIDArticle} не столбец, отсутствует <td>...</td>')
+                    print(f'{Fore.RED}{find_id_article} не столбец, отсутствует <td>...</td>')
                     Article = ""
                 else:
                     if gllogon >= 2:
                         print(f'Del ({ttd[0]}; {ttd[(len(ttd) - 1)]})')
-                    FindIDArticle = ClearDescription(FindIDArticle.lstrip(ttd[0]).rstrip(ttd[(len(ttd) - 1)]))
-                    Article = FindIDArticle
+                    find_id_article = ClearDescription(find_id_article.lstrip(ttd[0]).rstrip(ttd[(len(ttd) - 1)]))
+                    Article = find_id_article
                     if gllogon >= 2:
-                        print(f'{FindIDArticle=}')
+                        print(f'{find_id_article=}')
 
                 # Article = Article.replace()
                 dgt = re.findall('(\d+)', Article)
-                ArticleId = Article
+                article_id = Article
                 if len(dgt) > 0:
-                    ArticleId = dgt[0]
+                    article_id = dgt[0]
                     if gllogon >= 1:
-                        print(f'{ArticleId=}')
-                if ArticleId.isdigit():
-                    Id = int(ArticleId)
+                        print(f'{article_id=}')
+                if article_id.isdigit():
+                    Id = int(article_id)
                     if Id > 99999:
-                        ArticleFle = f'Article{ArticleId}.html'
+                        ArticleFle = f'Article{article_id}.html'
                         content = f'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n<html>\n'
-                        content = f'{content}\n<head><title>{ArticleId}</title>\n<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n</head>'
+                        content = f'{content}\n<head><title>{article_id}</title>\n<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n</head>'
                         content = f'{content}\n<style>table, th, td {{border: 1px solid black; border-collapse: collapse;}}</style>\n<body>'
-                        content = f'{content}\n<table width="80%">\n<tbody>\n{headRow}\n{htmlRow}\n</tbody>\n</table>\n'
+                        content = f'{content}\n<table width="80%">\n<tbody>\n{head_row}\n{html_row}\n</tbody>\n</table>\n'
                         content = f'{content}\n</body>\n</html>'
                         content = ClearDescription(content)
-                        exportFileName = os.path.join(downloadDirectory, ArticleFle)
-                        WriteHtml(content, exportFileName)
+                        export_file_name = os.path.join(download_directory, ArticleFle)
+                        WriteHtml(content, export_file_name)
                     else:
                         print(f'{Fore.RED}HelpID меньше 999999, ({Id})')
                 else:
-                    print(f'{Fore.RED}HelpID не число, ({ArticleId})')
+                    print(f'{Fore.RED}HelpID не число, ({article_id})')
 
 
 def main():
@@ -205,45 +202,53 @@ def main():
     print(f'{Fore.CYAN}{currentDirectory=}')
     print(f'{Fore.CYAN}{gllogon=}')
 
-    downloadDirectory = os.path.join(currentDirectory, "Dowload")
-    if not os.path.exists(downloadDirectory):
-        os.makedirs(downloadDirectory)
-        print(f"{Fore.CYAN}{downloadDirectory=}")
+    download_directory = os.path.join(currentDirectory, "Dowload")
+    if not os.path.exists(download_directory):
+        os.makedirs(download_directory)
+        print(f"{Fore.CYAN}{download_directory=}")
     print()
     print(f'{Fore.CYAN}===========================================================================')
     indx = 0
 
     for curId in idList:
-        if curId != "":
-            indx = indx + 1
-            print(f'{Fore.GREEN}Process {indx}: {curId=}')
+        try:
+            if curId != "":
+                indx = indx + 1
+                print(f'{Fore.GREEN}Process {indx}: {curId=}')
 
-            if curId.isdigit():
-                print(f'Get RedMine Issue Description: {curId=}')
+                if curId.isdigit():
+                    print(f'Get RedMine Issue Description: {curId=}')
+                    # 1 Get RedMine Issue Description
+                    issue = redmine.issue.get(curId, include=['watchers'])
+                    issue_description = issue.description
+                    try:
+                        ProcessDescription(download_directory, issue_description)
+                    except Exception as err:
+                        print(f'{Fore.RED}Some Issue error...\n{err}')
 
-                # 1 Get RedMine Issue Description
-                issue = redmine.issue.get(curId, include=['watchers'])
-                issueDescription = issue.description
-                ProcessDescription(downloadDirectory, issueDescription)
-
-                print(f'Количество наблюдателей = {len(issue.watchers)}')
-                for user in issue.watchers:
-                    usr = redmine.user.get(user.id)
-                    print(f'{user} = {usr.mail}')
-            else:
-                print(f'Get RedMine Wiki Description: {curId=}')
-
-                wikiId = curId.split('/')
-                if len(wikiId) == 3:
-                    projectId = wikiId[0]
-                    wikiName = wikiId[2]
-
-                    # 1 Get RedMine Wiki Description
-                    wiki = redmine.wiki_page.get(wikiName, project_id=projectId, include=[])
-                    wikiDescription = wiki.text
-                    ProcessDescription(downloadDirectory, wikiDescription)
+                    print(f'Количество наблюдателей = {len(issue.watchers)}')
+                    for user in issue.watchers:
+                        usr = redmine.user.get(user.id)
+                        print(f'{user} = {usr.mail}')
                 else:
-                    print(f"Skip wiki {wikiId=}")
+                    print(f'Get RedMine Wiki Description: {curId=}')
+
+                    wiki_id = curId.split('/')
+                    if len(wiki_id) == 3:
+                        project_id = wiki_id[0]
+                        wiki_name = wiki_id[2]
+
+                        # 1 Get RedMine Wiki Description
+                        wiki = redmine.wiki_page.get(wiki_name, project_id=project_id, include=[])
+                        wiki_description = wiki.text
+                        try:
+                            ProcessDescription(download_directory, wiki_description)
+                        except Exception as err:
+                            print(f'{Fore.RED}Some Wiki error...\n{err}')
+                    else:
+                        print(f"Skip wiki {wiki_id=}")
+        except Exception as err:
+            print(f'{Fore.RED}Some "{curId}" error...\n{err}')
 
         print(f'{Fore.CYAN}_________________________________________________________')
 
@@ -252,14 +257,14 @@ def main():
 
 
 if __name__ == "__main__":
-    print(f"{Fore.CYAN}Last update: Cherepanov Maxim masygreen@gmail.com (c), 07.2022")
+    print(f"{Fore.CYAN}Last update: Cherepanov Maxim masygreen@gmail.com (c), 07.2022-11.2023")
     print(f"{Fore.CYAN}Convert table to *.html")
 
     currentDirectory = os.getcwd()
     configFilePath = os.path.join(currentDirectory, 'config.cfg')
 
     if ReadConfig(configFilePath):
-        idList = glid.split(';')
+        idList = glid
         print(f'{idList=}')
 
         main()
